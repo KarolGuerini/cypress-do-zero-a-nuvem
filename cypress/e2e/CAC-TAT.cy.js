73d1d2657765151e0541cfe2fc9691485a95944a
KarolGuerini <karoltech20@gmail.com>
2025-07-08 12:14:52 -0300

    testes da licao 2 finalizados

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,15 +1,16 @@
-/// <reference types="cypress" />
+/ <reference types="cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', () => {
+    beforeEach(()=>{  //lição 2
+    cy.visit('./src/index.html')
+  })
 
   it('verifica o título da aplicação', () => {  //lição 1
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
   })
-  beforeEach(()=>{  //lição 2
-    cy.visit('./src/index.html')
-  })
-  it('preenche os campos obrigatórios e envia o formulário',()=>{
-    const logText = Cypress._.repeat('abcdefghijklmnopqrstuwxyz', 10)//Cypress._.repeat uma biblioteca que repete a string um número de vezes
+
+  it('preenche os campos obrigatórios e envia o formulário',()=>{ //lição 2
+    const logText = Cypress._.repeat('abcdefghijklmnopqrstuwxyz', 10)//Cypress._.repeat é uma biblioteca que repete a string um número de vezes
 
     cy.get('#firstName').type('Karol')
     cy.get('#lastName').type('Guerini')
@@ -17,19 +18,24 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('#open-text-area').type(logText, {delay: 0})//delay é como se fosse copia e cola
     cy.get('.button[type="submit"]').click()
 
-    cy.get('.success').should('be.visible')
+    cy.get('.success')
+    .should('be.visible')
   })
-  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', ()=>{
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', ()=>{ //lição 2
+
     cy.get('#firstName').type('Karol')
     cy.get('#lastName').type('Guerini')
     cy.get('#email').type('karoltesteexemplo.com')
     cy.get('#open-text-area').type('teste')
-    cy.get('.button[type="submit"]').click()
+    cy.contains("button", "Enviar").click()
+    // cy.get('.button[type="submit"]').click() usando o .get para buscar um elemento no DOM e clicar nele
 
-    cy.get('.error').should('be.visible')
+
+    cy.get('.error')
+    .should('be.visible')
   })
 
-  it('campo telefone continua vazio quando preenchido com um valor não numérico', ()=>{
+  it('campo telefone continua vazio quando preenchido com um valor não numérico', ()=>{ //lição 2
     cy.get('#phone')
     .as('phone')
     .type('abcde')
@@ -37,18 +43,21 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     .should('have.value', '')
   })
 
-  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', ()=>{
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', ()=>{ //lição 2
+
     cy.get('#firstName').type('Karol')
     cy.get('#lastName').type('Guerini')
     cy.get('#email').type('karoltesteexemplo.com')
     cy.get('#phone-checkbox').click()
     cy.get('#open-text-area').type('teste')
-    cy.get('.button[type="submit"]').click()
+    cy.contains("button", "Enviar").click()
 
-    cy.get('.error').should('be.visible')
+    cy.get('.error')
+    .should('be.visible')
   })
 
-  it('preenche e limpa os campos nome, sobrenome, email e telefone', ()=>{
+  it('preenche e limpa os campos nome, sobrenome, email e telefone', ()=>{ //lição 2
+
     cy.get('#firstName')
      .type('Karol')
      .clear()
@@ -67,16 +76,40 @@ describe('Central de Atendimento ao Cliente TAT', () => {
      .should('have.value', '')
   })
 
-it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', ()=>{
-  cy.get('.button[type="submit"]').click()
+it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', ()=>{ //lição 2
+
+    cy.contains("button", "Enviar").click()
 
   cy.get('.error')
    .should('be.visible')
 })
 
-it('envia o formuário com sucesso usando um comando customizado', ()=>{
+it('envia o formuário com sucesso usando um comando customizado', ()=>{ //lição 2
   cy.fillMandatoryFieldsAndSubmit()
   
-    cy.get('.success').should('be.visible')
+    cy.get('.success')
+    .should('be.visible')
+})
+
+it('envia o formuário com sucesso usando um comando customizado passando um argumento', ()=>{ //lição 2
+  const data = {
+    firstName: 'Karol',
+    lastName: 'Guerini',
+    email: 'kg@teste.com',
+    text: 'Testando 123'
+  }
+  cy.fillMandatoryFieldsAndSubmitWithData(data)
+  
+    cy.get('.success')
+    .should('be.visible')
+})
+
+
+it('envia o formuário com sucesso usando um comando customizado passando um valor padrão', ()=>{ //lição 2
+
+  cy.fillMandatoryFieldsAndSubmitWithDataDefault()
+  
+    cy.get('.success')
+    .should('be.visible')
 })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,5 +3,26 @@ Cypress.Commands.add('fillMandatoryFieldsAndSubmit', () => {
     cy.get('#lastName').type('Guerini')
     cy.get('#email').type('karolteste@exemplo.com')
     cy.get('#open-text-area').type('Teste')
-    cy.get('.button[type="submit"]').click()
+    cy.contains("button", "Enviar").click()
+})
+
+Cypress.Commands.add('fillMandatoryFieldsAndSubmitWithData', data => {
+    cy.get('#firstName').type(data.firstName)
+    cy.get('#lastName').type(data.lastName)
+    cy.get('#email').type(data.email)
+    cy.get('#open-text-area').type(data.text)
+    cy.contains("button", "Enviar").click()
+})
+
+Cypress.Commands.add('fillMandatoryFieldsAndSubmitWithDataDefault', (data ={
+    firstName: "Jonh",
+    lastName: "Doe",
+    email: "jonhdoe@teste.com",
+    text: "Teste Default"
+}) => {
+    cy.get('#firstName').type(data.firstName)
+    cy.get('#lastName').type(data.lastName)
+    cy.get('#email').type(data.email)
+    cy.get('#open-text-area').type(data.text)
+    cy.contains("button", "Enviar").click()
 })
